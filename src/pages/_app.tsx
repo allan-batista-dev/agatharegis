d3457a54b9cc80626097fcd2395c24842035f012
Allan Batista <macallan@MacBook-Air-de-Allan.local>
2023-05-12 13:39:30 -0300

    vercel commit-a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navigation from '../components/Navigation'
+import { SessionProvider, useSession } from "next-auth/react"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  // const { data: session, status} = useSession();
+
+  return (
+    <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
