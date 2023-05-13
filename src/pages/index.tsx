904a8998acb4c92196ad8f5a056b3f053e86c662
Allan Batista <macallan@MacBook-Air-de-Allan.local>
2023-05-13 17:15:25 -0300

    vercel commit-a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 export default function Home() {
 
-  return (
-<h1>agatha</h1>
-  );
+    return (
+        <>
+            <main>
+                <div className="flex justify-center">
+                    <div className="my-6">
+                        <Image
+                        src={"/../public/img/agatha.jpeg"}
+                        alt="agatha"
+                        width={200}
+                        height={10}
+                        className="rounded-full"
+                    />
+                    </div>
+                </div>
+                <div className="mb-6 text-center">
+                        <h1>Agatha Regis</h1>
+                        <p>sldkjalsdjalksjdlkajslk</p>
+                    </div>
+            </main>
+        </>
+    );
 }
 
