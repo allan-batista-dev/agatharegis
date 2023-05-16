3a55049258c9fc3cae9116534660816a5b19c0ab
Allan Batista <macallan@MacBook-Air-de-Allan.local>
2023-05-16 15:50:14 -0300

    layout init background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider, useSession } from "next-auth/react"
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
         <Component {...pageProps} />
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,45 @@
 import Image from "next/image";
-export default function Home() {
+import Head from "next/head";
+import agathaImage from "../../public/img/agatha.jpeg"
+import bgImage from "../../public/img/bg1.png"
 
-    return (
-        <>
-            <main>
-                <div className="flex justify-center">
-                    <div className="my-6">
-                        <Image
-                        src={"/../public/img/agatha.jpeg"}
-                        alt="agatha"
-                        width={200}
-                        height={10}
-                        className="rounded-full"
-                    />
-                    </div>
-                </div>
-                <div className="mb-6 text-center">
-                        <h1>Agatha Regis</h1>
-                        <p>sldkjalsdjalksjdlkajslk</p>
-                    </div>
-            </main>
-        </>
-    );
+export default function Home() {
+	return (
+		<>
+			<main>
+				<Image
+					className="bgImage"
+					src={bgImage}
+					alt="bg"
+					layout="fill"
+					objectFit="cover"
+					objectPosition="center"
+					priority
+				/>
+				<Head>
+					<title>Agatha Regis | Estética Personalizada</title>
+				</Head>
+				<div className="flex justify-center">
+					<div className="my-6">
+						<Image
+							src={agathaImage}
+							alt="agatha"
+							priority
+							width={200}
+							height={10}
+							className="rounded-full"
+						/>
+					</div>
+				</div>
+				<div className="mb-6 text-center">
+					<h1 className="text-4xl mb-2">Agatha Regis</h1>
+					<p className="text-1xl">Estética Personalizada</p>
+				</div>
+				<div>
+
+				</div>
+			</main>
+		</>
+	);
 }
 
